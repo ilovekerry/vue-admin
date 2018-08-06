4244bf8ce51abe381e64dd3441530c1ab9e49b8f
xianxiaobo <1015207497@qq.com>
2018-08-06 18:37:59 +0800

    add loginCard

---
+++ src/components/user/component/loginCard.vue
@@ -0,0 +1,63 @@
+<style lang="scss">
+
+</style>
+
+<template>
+	<div class="login" @keydown.enter="handleSubmit">
+		<div class="login-con">
+			<Card :bordered="false">
+				<div class="form-con">
+					<Form ref="loginForm" :model="userInfo">
+						<FormItem prop="userName">
+							<Input v-model="userInfo.username" :placeholder="nameLabel">
+							<span slot="prepend">
+                                    <Icon :size="16" type="person"></Icon>
+                                </span>
+							</Input>
+						</FormItem>
+						<FormItem prop="password">
+							<Input type="password" v-model="userInfo.password" :placeholder="passwordLabel">
+							<span slot="prepend">
+                                    <Icon :size="14" type="locked"></Icon>
+                                </span>
+							</Input>
+						</FormItem>
+						<FormItem>
+							<Button @click="handleSubmit" type="primary" long>登录</Button>
+						</FormItem>
+					</Form>
+				</div>
+			</Card>
+		</div>
+	</div>
+</template>
+
+<script>
+
+	export default {
+		name: 'loginCard',
+		props: {
+			nameLabel: String,
+			passwordLabel: String,
+			initialUserInfo: {
+				type: Object,
+				default: {
+					username:"",
+					password:"",
+				}
+			},
+			submit: {}
+		},
+		data: function () {
+			return {
+				userInfo: this.initialUserInfo
+			}
+		},
+		methods: {
+			handleSubmit(){
+				this.submit(this.userInfo);
+			}
+		}
+	};
+</script>
+
--- src/components/user/user.vue
+++ src/components/user/user.vue
@@ -1,11 +1,39 @@
 <template>
-  <div>user works</div>
+	<div>
+		user works;
+		<login-card :initialUserInfo="params" :submit='loginTo' nameLabel="用户名" passwordLabel="密码"></login-card>
+	</div>
 </template>
 
 <script>
-export default {
-    name: "user"
-};
+	import loginCard from "./component/loginCard.vue";
+	export default {
+		name: "login",
+		components:{
+			loginCard
+		},
+		data () {
+			return {
+				params: {
+					username:"admin",
+					password:"password",
+				},
+				loginTo: function (params) {
+					console.log(params);
+					console.log(params.username,params.password);
+				}
+			}
+		},
+		methods: {
+//			loginTo: function (params) {
+//				loginService.login(params).then(res => {
+//					if(res){
+//						console.log(res);
+//					}
+//				});
+//			},
+		}
+	};
 </script>
 
 <style scoped>
